1e8140f7e9c27e3a4e3421283c9cfbb04531a1a1
kanoi-y <furafuratonbo@gmail.com>
2022-10-13 23:07:06 +0900

    🔨 v-bind,computed,v-ifを記述

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,35 @@
+<script>
+export default {
+  data() {
+    return {
+      title: 'My New Vue Title',
+      message: 'Welcome to Vue',
+      isRed: true,
+      user: {
+        firstName: 'John',
+        lastName: 'Smith',
+        isMember: true,
+      },
+    }
+  },
+  computed: {
+    fullName() {
+      return this.user.firstName + ' ' + this.user.lastName
+    },
+  },
+}
+</script>
+
 <template>
-  <h1>My Vue Playground</h1>
+  <h1 :title="message" :class="{ red: isRed }">{{ title }}</h1>
+  <h2>{{ fullName }}さんのデータ</h2>
+  <p>Name: {{ fullName }}</p>
+  <p v-if="user.isMember">メンバーです</p>
+  <p v-else>メンバーではありません</p>
 </template>
+
+<style>
+.red {
+  color: red;
+}
+</style>
